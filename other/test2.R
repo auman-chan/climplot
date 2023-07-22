eca59cc40d0ce242689f334c0c40ae3cc2932029
Yaniel Wong <czhfreelancer@163.com>
2023-07-22 21:35:39 +0800

    add xlsx for test

--- other/test2.R
+++ other/test2.R
@@ -13,7 +13,8 @@ library(sp)
 data("locdata")
 data("plotdata_Frost")
 
-
+data("locdata")
+y <- readxl::read_xlsx("other/datatest.xlsx")
 m <- data.frame(No="1",location="test",lon=113.27,lat=23.13,altitude=20)
 p <- as_tibble(m)
 loc <- as.data.frame(locdata)
@@ -21,8 +22,14 @@ a <- "D:/climplot/tmin"
 b <- "D:/climplot/tmax"
 c <- "D:/climplot/prec"
 d <- "D:/climplot/extmin"
-x <- clim_extract(locdata,a,b,c,Frost = T,d)
-loc <- as_tibble(locdata)
-file <- m
-avmintemp <- list.files(a,full.names = T)
-x <- raster::stack(avmintemp)
+x <- clim_extract(y,a,b,c,Frost = T,d)
+clim_plot(x,ShowFrost = T)
+
+for (i in 1:3){
+
+ sub <- subset(x,No==i)
+ clim_plot(data=sub,ylabel = TRUE,
+        ylab1="Temperature(\U{00B0}C)",
+            ylab2="Precipitation(mm)",
+            p50line = TRUE)
+}
