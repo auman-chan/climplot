413e3dd44c519d9dcc6bc6dcce076bc65a7a2014
Yaniel Wong <czhfreelancer@163.com>
2023-07-26 22:39:27 +0800

    <test>: discard test of plot temporarily.

--- tests/testthat/test-clim_plot.R
+++
@@ -1,9 +0,0 @@
-test_that("plot", {
-  data("plotdata")
-  x <- subset(plotdata, No == 2)
-  climplot::clim_plot(x)
-  path <- tempdir()
-  png(filename = file.path(path, "plot.png"))
-  exp <- list.files(tempdir(), pattern = "plot.png",full.names = TRUE)
-  expect_true(file.exists(exp))
-})
