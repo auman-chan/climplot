bfdee11c844d40e7794acd8d576920ddfd9d1435
Yaniel Wong <czhfreelancer@163.com>
2023-07-27 16:57:43 +0800

    <tests>: change "Cairo" to "png" cos macOS don't have x11.

--- tests/testthat/test-clim_plot.R
+++ tests/testthat/test-clim_plot.R
@@ -1,11 +1,42 @@
 test_that("plot", {
   data("plotdata")
   x <- subset(plotdata, No == 2)
-  Cairo::CairoPNG( filename = "plot.png",
-                   width = 629,height = 548)
+  png(
+    filename = "plot.png",
+    width = 629, height = 548
+  )
   climplot::clim_plot(x)
   dev.off()
-announce_snapshot_file(path = "plot.png")
-announce_snapshot_file(path ="name.png")
-  compare_file_binary("plot.png","name.png")
+  announce_snapshot_file(path = "plot.png")
+  announce_snapshot_file(path = "name.png")
+  compare_file_binary("plot.png", "name.png")
+})
+
+test_that("plot of frost", {
+  data("plotdata")
+  x <- subset(plotdata, No == 1)
+  png(
+    filename = "plot.png",
+    width = 629, height = 548
+  )
+  climplot::clim_plot(x, showfrost = TRUE)
+  dev.off()
+  announce_snapshot_file(path = "plot.png")
+  announce_snapshot_file(path = "name2.png")
+  compare_file_binary("plot.png", "name2.png")
+})
+
+
+test_that("plot of labels", {
+  data("plotdata")
+  x <- subset(plotdata, No == 1)
+  png(
+    filename = "plot.png",
+    width = 629, height = 548
+  )
+  climplot::clim_plot(x, ylabel = TRUE, xlab = "M", ylab1 = "T", ylab2 = "P")
+  dev.off()
+  announce_snapshot_file(path = "plot.png")
+  announce_snapshot_file(path = "name3.png")
+  compare_file_binary("plot.png", "name3.png")
 })
